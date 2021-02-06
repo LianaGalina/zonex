f222eddcac4a6ac86addff12cd9eafa67633ef97
Liana <ms.lianagalina@mail.ru>
2021-02-06 20:46:23 +0500

    nav height

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -2,9 +2,10 @@ import vars from '../_vars';
 
 vars.$burger.addEventListener('click', (e) => {
     vars.$nav.classList.add('nav--visible');
-    vars.$body.style.overflow = "hidden";
+
 });
 
 vars.$navClose.addEventListener('click', (e) => {
     vars.$nav.classList.remove('nav--visible');
 })
+
