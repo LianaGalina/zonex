91e6167ce75a9e6b151855101535f18c8f8e57c8
Liana <ms.lianagalina@mail.ru>
2021-02-06 20:27:01 +0500

    added burger menu

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -2,6 +2,7 @@ import vars from '../_vars';
 
 vars.$burger.addEventListener('click', (e) => {
     vars.$nav.classList.add('nav--visible');
+    vars.$body.style.overflow = "hidden";
 });
 
 vars.$navClose.addEventListener('click', (e) => {
